f9d5d4674332db2bf1408cada8410540b82b4fa1
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-12-21 18:42:01 -0800

    add files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { SidebarProvider } from './components/ui/sidebar'
+
+function App() {
+  return (
+    <SidebarProvider>
+      
+    </SidebarProvider>
+  )
+}
+
+export default App
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,29 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import { SquarePenIcon } from "lucide-react"
+
+const items = [
+  {
+    title: "New chat",
+    href: "#",
+    icon: SquarePenIcon
+  },
+  
+]
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
